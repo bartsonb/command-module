147074c83c6a568fb05d959859bea6a52876d5c9
Armin Bartnik <armin.bartnik@web.de>
2020-02-21 18:09:51 +0100

    added all necessary dependencies and added JSDOC documentation

--- app.js
+++ app.js
@@ -1,49 +1,111 @@
-// let boat = require('boat');
-// let at = require('at');
-// let gps = require('gps');
+const { navigationHandler } = require("boat-simulation/dist/navigationHandler");
+const { BoatControl } = require('motorControl');
+const { start, stop, post } = require('communication/gsm');
+const getPosition = require('communication/gps');
+const getDetectedWaste = require('obstacle-detector');
 
-const HEARTBEAT_RATE = 5000;
-const COMMAND_EXECUTION_RATE = 1000;
+const NAVIGATION_RATE = 100;
+const HEARTBEAT_RATE = 3000;
+
+let port = null;
+
+let intervals = {
+    navigation: null,
+    heartbeat: null
+};
 
 let status = {
     fence: [],
-    position: [], 
-    commands: []
-}
-
-let setParams = ({ fence, position, commands }) => {
-    if (valid(fence)) // Boat.fence = fence;
-
-    if (valid(position)) // Boat.position = position; 
-
-    if (valid(commands)) {
-        status.commands = commands;
-        executeCommand();
-    }
+    wayPoints: [],
+    control: BoatControl,
+    detector: null,
+    command: "STOP",
+    position: [],
+    speed: 0,
+    heading: 0
 };
 
-let executeCommand = () => {
-    if (status.commands.includes('STOP')) status.commands = ['STOP'];
+/**
+ * @desc    Sets parameter to the status object after validating them.
+ */
+let setParams = ({ fence, wayPoints, command, position, speed, heading }) => {
+    if (valid(fence)) status.fence = fence;
 
-    switch(status.commands.unshift()) {
-        case 'SEARCH':
-            // Boat.search()
-            break;
+    if (valid(wayPoints)) status.wayPoints = wayPoints;
 
-        case 'RETURN':
-            // Boat.return()
-            break;
+    if (valid(position)) status.position = position;
 
-        case 'STOP':
-            // Boat.stop()
-            break;
-    }
+    if (typeof speed === "number") status.speed = speed;
+
+    if (typeof heading === "number") status.heading = heading;
+
+    if (typeof command === "string") status.command = command;
 };
 
+/**
+ * @desc    Gets the Waste Detection Object and calls the navigationHandler
+ *          if status has a valid position and fence.
+ */
+let navigate = () => {
+    status.detector = await getDetectedWaste('192.168.1.2:3000').catch(err => console.log(err));
+
+    if (valid(status.position) && valid(status.fence)) navigationHandler(status);
+};
+
+/**
+ *  @desc   Gets Drone position via gps and sends the data to our server, which
+ *          returns an available command, wayPoints and the fence.
+ */
+let heartbeat = () => {
+    clearInterval(intervals.heartbeat);
+
+    getPosition(port)
+        .then(res => {
+            setParams(res);
+
+            post(getKeys(["position", "heading", "speed", "clear"], status))
+                .then(res => setParams(res))
+                .then(res => {
+                    intervals.heartbeat = setInterval(heartbeat, HEARTBEAT_RATE);
+                    console.table(status);
+                });
+        })
+        .catch(err => console.log(err));
+};
+
+/**
+ * @desc    Removes all keys in the given object that are not listed in the given array.
+ * @param   validKeys: array
+ * @param   object: object
+ * @returns Object
+ */
+let getKeys = (validKeys, object) => {
+    let newObject = {};
+
+    Object.keys(object).forEach(key => {
+        if (validKeys.includes(key)) newObject[key] = object[key]
+    });
+
+    return newObject;
+};
+
+/**
+ * @desc    Checks if the given parameter is an array with a length greater than one.
+ * @param   arr: array
+ * @returns boolean
+ */
 let valid = (arr) => Array.isArray(arr) && arr.length > 0;
 
-let heartbeat = async () => {} // gsm.callApi();
+/**
+ * @desc    Init-function which starts the gsm module and sets off the heartbeat
+ *          and navigation interval.
+ */
+(function () {
+    start().then(_port => {
+        port = _port;
+        console.log('GSM MODULE STARTED ON PORT: ' + _port);
 
-setInterval(heartbeat, HEARTBEAT_RATE);
-
-setInterval(executeCommand, COMMAND_EXECUTION_RATE);
+        intervals.heartbeat = setInterval(heartbeat, HEARTBEAT_RATE);
+        intervals.navigation = setInterval(navigate, NAVIGATION_RATE);
+    })
+})();
