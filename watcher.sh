38823e0e6531439cf68909e2acb20ff408d61d25
Armin Bartnik <armin.bartnik@web.de>
2020-03-12 20:29:30 +0100

    added watcher script to track the latest logfile

---
+++ watcher.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# get pid of node process
+read node_process <<< $(pidof node)
+echo "Node process pid: ${node_process:-no process found}"
+
+cd logs || exit
+
+# Get the latest logfile
+#  -1 lists one file per line
+#  -r reverses output order (desc)
+read logfile <<< $(ls -1 -r *.log)
+
+# output data from logfile to console
+#  -f    append data as the file grows
+#  -n +1 output starting with line number given
+tail -f -n +1 ${logfile}
