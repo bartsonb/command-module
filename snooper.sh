473edd48ca8373a6d28d3ae2c8276fa19a2411dd
Armin Bartnik <armin.bartnik@web.de>
2020-04-03 05:54:41 +0200

    added fallbacks to bash scripts

--- snooper.sh
+++ snooper.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# Get pid of node process, if available
-read node_process <<< $(pidof node)
-echo "Node process pid: ${node_process:-no node process detected}"
+# Get pid of node process
+read node_pid <<< $(pidof node)
+printf "[Node PID]: ${node_pid:-No node process found.}\n\n"
+
+# Exiting if no node process was found
+[ -z ${node_pid} ] && exit
 
 # Start strace to get node process output
 # -f    folow forks, child processes
@@ -10,4 +13,4 @@ echo "Node process pid: ${node_process:-no node process detected}"
 # 2>&1  pipes the strace stderr output to stdout
 # -o    only returns the match
 # -P    Pattern is Perl regular expressions
-sudo strace -p${node_process} -f -s9999 -e write 2>&1 | grep -oP '(?<=")(\d{4}.+?)(?=\\n)'
+sudo strace -p${node_pid} -f -s9999 -e write 2>&1 | grep -oP '(?<=")(\d{4}.+?)(?=\\n)'
--- watcher.sh
+++ watcher.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-# get pid of node process, if available
-read node_process <<< $(pidof node)
-echo "Node process pid: ${node_process:-no process found}"
-
+# Checking if logs folder exists
 cd logs || exit
 
-# Get the latest logfile filename
-#  -1 lists one file per line
-#  -r reverses output order (desc)
-read logfile <<< $(ls -1 -r *.log)
+# Checking if any log files exist
+#  -z checks if string is empty
+#  2> redirects stderr to file
+if [ -z "$(ls *.log 2> /dev/null)" ]
+then
+    printf "[Logfile]: No logfile found.\n" && exit
+else 
+    # Get the latest logfile filename
+    #  -1 lists one file per line 
+    #  -r reverses output order (desc)
+    read logfile <<< $(ls -1 -r *.log)
+    printf "[Logfile]: ${logfile}\n\n"
 
-# output data from logfile to console
-#  -f    append data as the file grows
-#  -n +1 output starting with line number given
-tail -f -n +1 ${logfile}
+    # output data from logfile to console
+    #  -f    append data as the file grows
+    #  -n +1 output starting with line number given
+    tail -f -n +1 ${logfile}
+fi
