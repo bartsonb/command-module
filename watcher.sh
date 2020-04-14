51f400d9a552593f075dae6410ffdf52396cc6ec
Armin Bartnik <armin.bartnik@web.de>
2020-04-14 12:12:49 +0200

    restructed & removed unnecessary ls call

--- watcher.sh
+++ watcher.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 
-# Checking if logs folder exists
-cd logs || exit
+# Get the latest logfile filename
+#  -1 lists one file per line 
+#  -r reverses output order (desc)
+#  2> redirects stderr to file, deleting error output
+read logfile <<< $(ls -A -1 -r logs/*.log 2> /dev/null)
 
-# Checking if any log files exist
+# Checking if a log file exist
 #  -z checks if string is empty
-#  2> redirects stderr to file
-if [ -z "$(ls *.log 2> /dev/null)" ]
-then
-    printf "[Logfile]: No logfile found.\n" && exit
-else 
-    # Get the latest logfile filename
-    #  -1 lists one file per line 
-    #  -r reverses output order (desc)
-    read logfile <<< $(ls -1 -r *.log)
-    printf "[Logfile]: ${logfile}\n\n"
+[[ -z ${logfile} ]] && printf "[Logfile]: No logfile found.\n" && exit || printf "[Logfile]: ${logfile}\n\n"
 
-    # output data from logfile to console
-    #  -f    append data as the file grows
-    #  -n +1 output starting with line number given
-    tail -f -n +1 ${logfile}
-fi
+# Output data from logfile to console
+#  -f    append data as the file grows
+#  -n +1 output starting with line number given
+tail -f -n +1 ${logfile}
